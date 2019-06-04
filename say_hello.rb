56a67d7ae9ec5e379c88cc80b5ba337d55c7b145
John Jordan Shelley <jshelley553@gmail.com>
2019-06-04 03:11:41 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,12 @@
 def say_hello(name)
 
-  puts "Hello #{name}!"
+  if name == nil
+    puts "Hello Ruby Programmer!"
+  else
+    puts "Hello #{name}!"
+  end 
 
 end
 
 print say_hello("Jordan")
+print say_hello()
